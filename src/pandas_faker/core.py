e19dc9eff428d427009e9b6b660ada25bd05f5dc
Bruno Amaral <amaralbf@gmail.com>
2022-03-21 00:10:07 -0300

    Make simple_df return a df with multiple rows and cols

--- src/pandas_faker/core.py
+++ src/pandas_faker/core.py
@@ -1,5 +1,9 @@
+import numpy as np
 import pandas as pd
 
 
 def simple_df() -> pd.DataFrame:
-    return pd.DataFrame()
+    columns = 'A B C D'.split()
+    n_rows = 5
+    data = np.arange(n_rows * len(columns)).reshape(n_rows, len(columns))
+    return pd.DataFrame(data=data, columns=columns)
--- tests/test_core.py
+++ tests/test_core.py
@@ -6,3 +6,9 @@ import pandas_faker as pf
 def test_simple_df_return_type():
     df = pf.simple_df()
     assert isinstance(df, pd.DataFrame)
+
+
+def test_simple_df_has_multiple_cols_and_rows():
+    df = pf.simple_df()
+    rows, cols = df.shape
+    assert rows > 1 and cols > 1
